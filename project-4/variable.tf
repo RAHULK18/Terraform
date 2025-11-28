bd318a8e579577819845aa05d8ec4ee844fc1d04
Rahul Karmakar <rahulkarmakar@ip-192-168-0-101.us-west-2.compute.internal>
2025-11-29 01:10:33 +0530

    Added mini-project-4

---
+++ project-4/variable.tf
@@ -0,0 +1,25 @@
+variable "ec2_aws_instance_type" {
+  default = "t2.micro"
+  type = string
+}
+
+variable "ec2_root_block_size" {
+  default = "10"
+  type = number
+}
+
+variable "ec2_ami_id" {
+  default = "ami-00f46ccd1cbfb363e"
+  type = string
+}
+variable "ec2_az" {
+  default = "us-west-2c"
+  type = string
+}
+
+
+variable "env" {
+  default = "prd"
+  type = string
+
+}
